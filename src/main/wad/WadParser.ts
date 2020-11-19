c56f41bc9ec3b5c22f72233ee64165784d504ed4
Maciej Miklas <mac.miklas@gmail.com>
2020-11-19 07:39:09 +0100

    dev

--- src/main/wad/WadParser.ts
+++ src/main/wad/WadParser.ts
@@ -1,8 +1,11 @@
+// Doc copied from https://doomwiki.org/wiki/WAD
 import * as R from "ramda";
 
-const parseStr = (pos: number, length: number) => (bytes: number[]): string => {
-    return String.fromCharCode.apply(null, bytes.slice(pos, pos + length))
-}
+const trim0Pos = (bytes: number[], pos: number) =>
+    R.until((v: number) => bytes[v] !== 0, (v: number) => v - 1)(pos) + 1
+
+const parseStr = (pos: number, length: number) => (bytes: number[]): string =>
+    String.fromCharCode.apply(null, bytes.slice(pos, trim0Pos(bytes, pos + length - 1)))
 
 /** little-endian */
 const parseInt = (pos: number) => (bytes: number[]): number => {
@@ -13,37 +16,64 @@ const parseInt = (pos: number) => (bytes: number[]): number => {
     )(bytes)
 }
 
-/** The ASCII characters "IWAD" or "PWAD" */
-const parseHeadType = parseStr(0x00, 4)
-
-/** An integer specifying the number of lumps in the WAD. */
-const parseHeadNumlumps = parseInt(0x04)
-
-/** An integer holding a pointer to the location of the directory. */
-const parseHeadInfotableofs = parseInt(0x08)
-
-/** A WAD file always starts with a 12-byte header. It contains three values. */
 const parseHeader = (bytes: number[]): Header => {
     return {
         // The ASCII characters "IWAD" or "PWAD".
-        identification: parseHeadType(bytes),
+        identification: WadType[parseStr(0x00, 4)(bytes)],
 
         // An integer specifying the number of lumps in the WAD.
-        numlumps: parseHeadNumlumps(bytes),
+        numlumps: parseInt(0x04)(bytes),
 
         // An integer holding a pointer to the location of the directory.
-        infotableofs: parseHeadInfotableofs(bytes)
+        infotableofs: parseInt(0x08)(bytes)
     };
 }
 
-// ############################ TYPES ############################
-type Header = {
-    identification: string,
-    numlumps: number,
-    infotableofs: number
+const parseDirectories = (header: Header, bytes: number[]): Directory[] => R
+    .unfold(idx => idx > header.numlumps ? false : [header.infotableofs + idx * 16, idx + 1], 0)
+    .map(ofs => parseDirectory(ofs, bytes))
+
+const parseDirectory = (offset: number, bytes: number[]): Directory => {
+    return {
+        filepos: parseInt(offset)(bytes),
+        size: parseInt(offset + 0x04)(bytes),
+        name: parseStr(offset + 0x08, 8)(bytes)
+    }
 }
 
 // ############################ TYPES ############################
+/** A WAD file always starts with a 12-byte header. */
+export type Header = {
+    identification: WadType,
+
+    /** An integer specifying the number of lumps in the WAD. */
+    numlumps: number,
+
+    /** An integer holding a pointer to the location of the directory. */
+    infotableofs: number
+}
+
+/**
+ * The directory associates names of lumps with the data that belong to them. It consists of a number of entries,
+ * each with a length of 16 bytes. The length of the directory is determined by the number given in the WAD header.
+ */
+export type Directory = {
+
+    /** An integer holding a pointer to the start of the lump's data in the file.  */
+    filepos: number,
+
+    /** An integer representing the size of the lump in bytes. */
+    size: number,
+
+    /** An ASCII string defining the lump's name. */
+    name: string
+}
+
+// ############################ TYPES ############################
+export enum WadType {
+    IWAD,
+    PWAD
+}
 
 class WadParser {
     static parse(bytes8: Uint8Array) {
@@ -51,5 +81,5 @@ class WadParser {
     }
 }
 
-export const testFunctions = {parseStr, parseInt, parseHeader, parseHeadType}
+export const testFunctions = {parseStr, parseInt, parseHeader, parseDirectory, parseDirectories}
 export default WadParser
--- src/test/WadParser.test.ts
+++ src/test/WadParser.test.ts
@@ -1,10 +1,34 @@
-import {testFunctions as tf} from "../main/wad/WadParser";
+import {Directory, Header, testFunctions as tf, WadType} from "../main/wad/WadParser";
 import {base64ToUint8Array} from "../main/util"
 import jsonData from "./data/doom.json"
 
 // @ts-ignore
 const wadBytes = base64ToUint8Array(jsonData.doom)
 const iwad = [73, 87, 65, 68]
+
+const firstDirectory: Directory = {
+    filepos: 12,
+    size: 10752,
+    name: "PLAYPAL"
+}
+
+const secondDirectory: Directory = {
+    filepos: 10764,
+    size: 8704,
+    name: "COLORMAP"
+}
+
+const sixDirectory: Directory = {
+    filepos: 69928,
+    size: 60372,
+    name: "DEMO3"
+}
+
+const lastDirectory: Directory = {
+    filepos: 0,
+    size: 0,
+    name: "F_END"
+}
 
 describe("parseInt", () => {
     test("12", () => {
@@ -16,11 +40,11 @@ describe("parseInt", () => {
     });
 
     test("1234567898", () => {
-        expect(tf.parseInt(0)([0xDA, 0x02,0x96,0x49])).toEqual(1234567898)
+        expect(tf.parseInt(0)([0xDA, 0x02, 0x96, 0x49])).toEqual(1234567898)
     });
 
     test("-999912", () => {
-        expect(tf.parseInt(0)([0x18,0xBE,0xF0,0xFF])).toEqual(-999912)
+        expect(tf.parseInt(0)([0x18, 0xBE, 0xF0, 0xFF])).toEqual(-999912)
     });
 });
 
@@ -42,17 +66,68 @@ describe("parseStr", () => {
     });
 });
 
-describe("parseHeadType", () => {
-    test("IWAD", () => {
-        expect(tf.parseHeadType(iwad)).toEqual("IWAD")
-    });
-});
-
 describe("parseHeader", () => {
     test("IWAD", () => {
         const header = tf.parseHeader(wadBytes);
-        expect(header.identification).toEqual("IWAD")
+        expect(header.identification).toEqual(WadType.IWAD)
         expect(header.numlumps).toEqual(1241)
         expect(header.infotableofs).toEqual(4205648)
     });
 });
+
+const validateLump = (header: Header) => (nr: number, given: Directory) => {
+    const dir = tf.parseDirectory(header.infotableofs + 16 * (nr - 1), wadBytes);
+    expect(dir.name).toEqual(given.name)
+    expect(dir.filepos).toEqual(given.filepos)
+    expect(dir.size).toEqual(given.size)
+}
+
+describe("parseDirectory", () => {
+    const header = tf.parseHeader(wadBytes);
+    const validate = validateLump(header);
+    test("First Lump", () => {
+        validate(1, firstDirectory)
+    });
+
+    test("Second Lump", () => {
+        validate(2, secondDirectory)
+    });
+
+    test("6-th Lump", () => {
+        validate(6, sixDirectory)
+    });
+
+    test("Last Lump", () => {
+        validate(header.numlumps, lastDirectory)
+    });
+})
+
+const findDirectory = (dir: Directory, dirs: Directory[]) =>
+    dirs.find(d => (d.name === dir.name && d.filepos === dir.filepos && d.size == dir.size))
+
+describe("parseDirectories", () => {
+    const header = tf.parseHeader(wadBytes);
+    const allDirs = tf.parseDirectories(header, wadBytes);
+    const validate = validateLump(header);
+
+    test("First Lump", () => {
+        const found = findDirectory(firstDirectory, allDirs)
+        validate(1, found)
+    });
+
+    test("Second Lump", () => {
+        const found = findDirectory(secondDirectory, allDirs)
+        validate(2, found)
+    });
+
+    test("6-th Lump", () => {
+        const found = findDirectory(sixDirectory, allDirs)
+        validate(6, found)
+    });
+
+    test("Last Lump", () => {
+        const found = findDirectory(lastDirectory, allDirs)
+        validate(header.numlumps, found)
+    });
+})
+
